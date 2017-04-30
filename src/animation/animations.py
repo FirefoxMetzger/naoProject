968926f1c3da26c65f4b71bad1e36a8f4ef6eb59
Samuel <samuelwestlake@hotmail.co.uk>
2017-04-30 15:45:04 +0100

    More animations.

--- src/animation/animations.py
+++ src/animation/animations.py
@@ -2,24 +2,80 @@
 
 import sys
 import math
-from time import sleep
 from naoqi import ALProxy
-from naoqi import ALModule
+import motion_dict
 
 
-class Animations(ALModule):
+class Animations(object):
 
-    def __init__(self, name):
-        ALModule.__init__(self, name)
+    def __init__(self, ip="127.0.0.1", port=9559):
         try:
-            self.motion = ALProxy("ALMotion")
+            self.motion = ALProxy("ALMotion", ip, port)
         except Exception, error:
             print "Could not create proxy to ALMotion"
             print "Error was: ", error
             sys.exit(1)
+
+    def face_palm(self):
+        self.run(motion_dict.face_palm)
+
+    def scratch_bum(self):
+        self.run(motion_dict.scratch_bum)
+
+    def tantrum(self):
+        self.run(motion_dict.tantrum)
+
+    def shake_head(self):
+        self.run(motion_dict.shake_head)
+
+    def nod_head(self):
+        self.run(motion_dict.nod_head)
+
+    def right_hand(self, pos, time):
+        self.motion.setStiffnesses("RHand", 0.9)
+        self.motion.angleInterpolation("RHand", pos, time, True)
+
+    def left_hand(self, pos, time):
+        self.motion.setStiffnesses("LHand", 0.9)
+        self.motion.angleInterpolation("LHand", pos, time, True)
+
+    def hands(self, pos, time):
+        self.motion.setStiffnesses("LHand", 0.9)
+        self.motion.setStiffnesses("RHand", 0.9)
+        self.motion.angleInterpolation(["LHand", "RHand"], [pos] * 2, [time] * 2, True)
+
+    def celebrate(self):
+        self.run(motion_dict.celebrate)
+
+    def wave(self):
+        self.run(motion_dict.raise_right_arm)
+        self.run(motion_dict.wave_right_arm)
+        self.run(motion_dict.lower_right_arm)
+
+    def run(self, motion):
+        [self.motion.setStiffnesses(joint, 0.9) for joint in motion["joints"]]
+        angle_lists = []
+        for joint, angles in zip(* (motion["joints"], motion["angles"])):
+            if "Hand" in joint:
+                angle_lists.append(angles)
+            else:
+                angle_lists.append([math.radians(angle) for angle in angles])
+        time_lists = [intervals2times(intervals) for intervals in motion["intervals"]]
+        self.motion.angleInterpolation(motion["joints"], angle_lists, time_lists, True)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exec_type, exec_value, traceback):
         return
+
+
+# Converts a lists of time intervals into a list of times
+def intervals2times(intervals):
+    n = len(intervals)
+    times = [sum(intervals[0:i + 1]) for i in range(n)]
+    return times
+
+if __name__ == "__main__":
+    a = Animations(port=35758)
+    a.scratch_bum()
---
+++ src/animation/motion_dict.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python
+
+raise_right_arm = {"joints": ["RShoulderPitch",
+                              "RShoulderRoll",
+                              "RElbowRoll",
+                              "RElbowYaw",
+                              "RWristYaw"],
+                   "angles": [[-25],
+                              [-20],
+                              [50],
+                              [110],
+                              [-85]],
+                   "intervals": [[1]] * 5}
+
+lower_right_arm = {"joints": ["RShoulderPitch",
+                              "RShoulderRoll",
+                              "RElbowRoll",
+                              "RElbowYaw",
+                              "RWristYaw"],
+                   "angles": [[85], [10], [23], [68], [-6]],
+                   "intervals": [[1]] * 5}
+
+shake_head = {"joints": ["HeadYaw"],
+              "angles": [[20, -20, 20, -20, 20, -20, 0]],
+              "intervals": [[0.2, 0.4, 0.4, 0.4, 0.4, 0.4, 0.2]]}
+
+nod_head = {"joints": ["HeadPitch"],
+            "angles": [[-5, 12, -5, 12, -5, 12, 0]],
+            "intervals": [[0.2, 0.4, 0.4, 0.4, 0.4, 0.4, 0.3]]}
+
+wave_right_arm = {"joints": ["RShoulderRoll",
+                             "RElbowYaw",
+                             "RHand"],
+                  "angles": [[20, -20, 20, -20],
+                             [70, 110, 70, 110],
+                             [1, 1, 1, 0.45]],
+                  "intervals": [[0.7, 0.7, 0.7, 0.7]] * 3}
+
+celebrate = {"joints": ["RShoulderPitch",
+                        "RShoulderRoll",
+                        "RElbowRoll",
+                        "RElbowYaw",
+                        "RWristYaw",
+                        "RHand"],
+             "angles": [[-17, 32, 3, 32, 32, 85],
+                        [14, 10, 7, 10, 10, 10],
+                        [55, 88, 70, 88, 88, 23],
+                        [95, 88, 88, 88, 88, 68],
+                        [70, 91, 91, 91, 91, -6],
+                        [1, 0, 0, 0, 0, 0.45]],
+             "intervals": [[1, 0.5, 0.5, 0.5, 0.7, 1]] * 6}
+
+face_palm = {"joints": ["HeadPitch",
+                        "HeadYaw",
+                        "RShoulderPitch",
+                        "RShoulderRoll",
+                        "RElbowRoll",
+                        "RElbowYaw",
+                        "RWristYaw",
+                        "RHand"],
+             "angles": [[20, 20, 0],
+                        [20, -20, 20, -20, 0],
+                        [23, 23, 85],
+                        [18, 18, 10],
+                        [83, 83, 24],
+                        [50, 50, 69],
+                        [62, 62, -6],
+                        [1, 1, 0.45]],
+             "intervals": [[1.5, 1, 0.3],
+                           [0.2, 0.4, 0.4, 0.4, 0.2],
+                           [1, 1, 1],
+                           [1, 1, 1],
+                           [1, 1, 1],
+                           [1, 1, 1],
+                           [1, 1, 1],
+                           [1, 1, 1]]}
+
+scratch_bum = {"joints": ["RHand"],
+               "angles": [[0.6, 1, 0, 1, 0, 1, 0.5]],
+               "intervals": [[0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]]}
+
+tantrum = {"joints": [],
+            "angles": [[]],
+            "intervals": [[]]}
+
+
+template = {"joints": [],
+            "angles": [[]],
+            "intervals": [[]]}
+
+
+template = {"joints": [],
+            "angles": [[]],
+            "intervals": [[]]}
+
+
+template = {"joints": [],
+            "angles": [[]],
+            "intervals": [[]]}
